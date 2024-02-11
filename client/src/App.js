658cc07e4b2777457c0202e845ea8f5d0d0c4b92
Raoof Naushad <raoof@farpointhq.com>
2024-02-11 03:49:22 -0400

    DEV

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+// Import the Sample component
+import Sample from "./pages/Sample/Sample"; // Make sure the path matches the location of your Sample.js file
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        {/* Render the Sample component as part of the app */}
+        <Sample />
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Developed by Blackholes
         </a>
       </header>
     </div>
